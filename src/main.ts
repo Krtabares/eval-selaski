e8ac068c331e12e5a6e7e41d076f3a8ba1ab1d96
krtabares <krtabares@gmail.com>
2024-09-20 09:51:23 -0400

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ecommerce API')
+    .setDescription('API para gestión de ecommerce')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
 
   // Habilitar CORS
   app.enableCors({
     origin: '*', // Permitir solicitudes desde cualquier origen
   });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
 
   await app.listen(3000);
 }
